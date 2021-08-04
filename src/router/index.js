464dfdf773d89399024051078771df0d1e890541
hollidavis <hollidavis19@gmail.com>
2021-08-04 14:02:22 -0600

    added job files

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import CarDetailsPage from '../pages/CarDetailsPage.vue'
+import JobDetailsPage from '../pages/JobDetailsPage.vue'
 import Home from '../pages/Home.vue'
+import Jobs from '../pages/Jobs.vue'
 
 
 const routes = [
@@ -9,10 +11,19 @@ const routes = [
     name: 'Home',
     component: Home
   },{
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  },{
     // required param ':id'
     path: '/car-details/:id',
     name: 'CarDetails',
     component: CarDetailsPage
+  },{
+    // required param ':id'
+    path: '/job-details/:id',
+    name: 'JobDetails',
+    component: JobDetailsPage
   }
 ]
 
